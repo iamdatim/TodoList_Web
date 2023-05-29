556fa2294d2ddb0d3652f7d2d8ff247110a34002
Dairo Timileyin <timileyin4baze@gmail.com>
2023-05-29 20:51:10 +0100

    done working on my login page

--- ToDoWeb/Controllers/LoginController.cs
+++ ToDoWeb/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using ToDoWeb.Data;
+using ToDoWeb.Models;
 
 namespace ToDoWeb.Controllers
 {
@@ -17,28 +19,31 @@ namespace ToDoWeb.Controllers
             return View();
         }
 
+
         [HttpPost]
         public IActionResult Login(string username, string password)
         {
             // Query the database to find the user
             var user = _db.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
 
-            if (user != null && VerifyPasswordHash(password, user.Password))
+            if (user != null)
             {
                 // Authentication successful
                 //FormsAuthentication.SetAuthCookie(username, false);
-                return RedirectToAction("Todo", "Todolist");
+                return RedirectToAction("Todolist", "Todo");
             }
 
             // Invalid credentials
             ViewBag.ErrorMessage = "Invalid username or password.";
-            return View();
+            return View("LoginPage");
 
         }
 
-        private bool VerifyPasswordHash(string password, string storedHash)
-        {
-            return BCrypt.Net.BCrypt.Verify(password, storedHash);
-        }
+        //private bool VerifyPasswordHash(string password, string storedHash)
+        //{
+        //    return BCrypt.Verify(password, storedHash);
+        //}
     }
+
+
 }
